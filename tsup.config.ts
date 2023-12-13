42e05f2dbaeb4243688463b6f8bdbba0c554818b
qwq <qwq@qwq.cat>
2023-12-13 17:59:57 +0800

    fix: remove `createRequire` banner

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,6 @@
-import dedent from 'dedent';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  banner: ({ format }) => {
-    if (format !== 'esm') return;
-
-    return {
-      js: dedent`
-        import { createRequire } from 'node:module';
-        const require = createRequire(import.meta.url);\n
-      `,
-    };
-  },
-
   clean: true,
   dts: { resolve: true },
   entry: ['src/*.{ts,tsx}'],
