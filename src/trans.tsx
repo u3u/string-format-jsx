b56046ffd14896a734358cb886618fd7b9a50b7f
qwq <qwq@qwq.cat>
2023-12-13 17:34:17 +0800

    chore: add examples for trans

--- src/trans.tsx
+++ src/trans.tsx
@@ -7,6 +7,44 @@ export interface TransProps extends InterpolationOptions {
   text: string;
 }
 
+/**
+ * @example
+ *   const result = (
+ *     <Trans
+ *       components={{
+ *         a: (children, key) => {
+ *           return (
+ *             <a
+ *               className="text-primary-500 hover:text-primary-400 transition"
+ *               key={key}
+ *               onClick={() => {
+ *                 console.log(children);
+ *               }}
+ *             >
+ *               {children}
+ *             </a>
+ *           );
+ *         },
+ *
+ *         b: (children, key) => {
+ *           return (
+ *             <b className="font-medium" key={key}>
+ *               {children}
+ *             </b>
+ *           );
+ *         },
+ *       }}
+ *       text={dedent`
+ *         Hello, <a>The number is <b>{count}</b></a>
+ *         <br />
+ *         The number is <b>{count}</b>.
+ *       `}
+ *       values={{
+ *         count: 10,
+ *       }}
+ *     />
+ *   );
+ */
 export const Trans = (props: TransProps) => {
   const { components, regex, text, values } = props;
 
